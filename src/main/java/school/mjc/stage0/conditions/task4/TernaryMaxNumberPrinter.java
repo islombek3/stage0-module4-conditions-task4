920b96c96399d7cc2ccc9d740f6fc761dc6d7d5f
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-23 21:55:00 +0500

    add logic

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first > second && second > third ? first : 
+        first > third && third > second ? first : 
+        second > first && first > third ? second :
+        second > third && third > first ? second :
+        third > first && first > second ? third :
+        third > second && second > first ? third :
+        first == second && first > third ? first :
+        first == second && first < third ? third :
+        first == third && first > second ? first :
+        first == third && first < second ? second :
+        second == third && first > second ? first :
+        first == third && first < second ? second : null; 
+        System.out.println(result);
     }
 }
