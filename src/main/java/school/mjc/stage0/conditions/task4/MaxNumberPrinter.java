362d6fa4667538832c128888e45ced0c605e00a8
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-23 21:53:25 +0500

    add logic

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,30 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first > second && second > third){
+            System.out.println(first);
+        } else if (first > third && third > second){
+            System.out.println(first);
+        } else if (second > first && first > third){
+            System.out.println(second);
+        } else if (second > third && third > first){
+            System.out.println(second);
+        } else if (third > first && first > second){
+            System.out.println(third);
+        } else if (third > second && second > first){
+            System.out.println(third);
+        } else if (first == second && first > third){
+            System.out.println(first);
+        } else if (first == second && first < third){
+            System.out.println(third);
+        } else if (first == third && first > second){
+            System.out.println(first);
+        } else if (first == third && first < second){
+            System.out.println(second);
+        } else if (second == third && first > second){
+            System.out.println(first);
+        } else if (first == third && first < second){
+            System.out.println(second);
+        }
     }
 }
